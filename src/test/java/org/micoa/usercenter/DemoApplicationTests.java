5ac2baaef7cf5286dc5b86283f7a48b6b89292ff
liuzhenyuan <liuzhenyuan@meituan.com>
2016-08-17 16:56:01 +0800

    add

--- src/test/java/org/micoa/usercenter/DemoApplicationTests.java
+++ src/test/java/org/micoa/usercenter/DemoApplicationTests.java
@@ -2,17 +2,27 @@ package org.micoa.usercenter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = UserCenterApplication.class)
+import static org.junit.Assert.assertNotNull;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
 @WebAppConfiguration
 public class DemoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull(applicationContext);
 	}
 
 }
